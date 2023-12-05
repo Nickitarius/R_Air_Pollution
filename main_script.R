ea8131f11339bf39fc5667bc520f7b9ec67f2ee3
Nickitarius <pilnov.nickita@yandex.ru>
2023-12-05 22:42:08 +0800

    Task 1 complete

--- main_script.R
+++ main_script.R
@@ -1,25 +1,29 @@
 
 getMean <- function(folder, type, id=1:332){
-  files_list <- list.files(folder)
-  # files_list <- files_list[which(file_path_sans_ext(basename(files_list)) )]
-  res = 0
-  
-  #имена файлов с нулями в начале
-  # file_names <- c()
+  values <- c()
   for (i in id){
     file_name = i
     if (i<10){
       file_name <- paste("00", file_name, sep="")
     } else if (i <100)
-    {file_name <- paste("0", file_name, sep="")}
+    {file_name <- paste("0", file_name, sep="")
+    }
     
     file_name <- paste(folder, "/", file_name, ".csv", sep="")
-    # file_names <- append(file_names, file_name)
     
+    file = read.csv(file_name)
     
+    #Удаляем NA
+    file <- file[, type]
+    file <- file[!is.na(file)]
+    
+    values <- c(values, file)
   }
-
+  mean(values)
 }
 
-getMean("data", "sulfate", 12:124)
+print("Testing function 1: ")
+getMean("data", "sulfate", 1:10)
+getMean("data", "nitrate", 70:72)
+getMean("data", "nitrate", 23)
 
