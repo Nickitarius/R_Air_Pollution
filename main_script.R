9ef6b7408895d6437a3fb1e677e50a7d309d1cab
Nickitarius <pilnov.nickita@yandex.ru>
2023-12-05 23:07:30 +0800

    2nd function added. Reading file is now a separate function

--- main_script.R
+++ main_script.R
@@ -1,22 +1,27 @@
+#Открывает и читает файл для других функций
+readFile <- function(folder, id){
+  file_name = id
+  
+  #Добавляем нули
+  if (id<10){
+    file_name <- paste("00", file_name, sep="")
+  } else if (id <100)
+  {file_name <- paste("0", file_name, sep="")
+  }
+  
+  file_name <- paste(folder, "/", file_name, ".csv", sep="")
+  
+  file <- read.csv(file_name)
+  file
+}
 
 getMean <- function(folder, type, id=1:332){
   values <- c()
   for (i in id){
-    file_name = i
-    if (i<10){
-      file_name <- paste("00", file_name, sep="")
-    } else if (i <100)
-    {file_name <- paste("0", file_name, sep="")
-    }
-    
-    file_name <- paste(folder, "/", file_name, ".csv", sep="")
-    
-    file = read.csv(file_name)
-    
-    #Удаляем NA
+    file <- readFile(folder, i)
     file <- file[, type]
+    #Удаляем NA
     file <- file[!is.na(file)]
-    
     values <- c(values, file)
   }
   mean(values)
@@ -26,4 +31,24 @@ print("Testing function 1: ")
 getMean("data", "sulfate", 1:10)
 getMean("data", "nitrate", 70:72)
 getMean("data", "nitrate", 23)
+
+getCompleteObservation <- function(folder, id = 1:332) {
+  df <- data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(df) <- c("id", "count")
+  
+  for (i in id){
+    file <- readFile(folder, i)
+    count <- dim(file[complete.cases(file),])[1]
+    df[nrow(df)+1, ] <- c(i, count)
+  }
+  df
+}
+
+print("Testing function 2: ")
+getCompleteObservation("data", 1)
+getCompleteObservation("data", c(2, 4, 8, 10, 12))
+getCompleteObservation("data", 30:25)
+getCompleteObservation("data", 3)
+
+
 
